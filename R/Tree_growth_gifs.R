b436d48705c064c0d021adaa1f973131e4907d43
Kah5 <kellyannheilman@gmail.com>
2021-09-28 16:46:02 -0700

    initial commit for code to create animated ggplot2 gifs of the tree growth

---
+++ R/Tree_growth_gifs.R
@@ -0,0 +1,64 @@
+# Script to make cross sectional images for the tree ring data:
+library(dplR)
+library(tidyverse)
+all.trees.info <- read.csv( "subset.musical.trees.info.csv")
+
+PIPO.q.rwl.wide <- read.csv( paste0("outputs/random_100_sorted/PIPO_AZ_rwl_wide.csv"))
+PIED.q.rwl.wide <- read.csv( paste0("outputs/random_100_sorted/PIED_AZ_rwl_wide.csv"))
+PSME.q.rwl.wide<- read.csv( paste0("outputs/random_100_sorted/PSME_AZ_rwl_wide.csv"))
+
+PIPO.q.rwl.wide <- PIPO.q.rwl.wide %>% filter(series %in% all.trees.info$TREE.ID.)
+colnames(PIPO.q.rwl.wide)[2:length(PIPO.q.rwl.wide)] <- 1719:1997
+PIPO.q.rwl.m <- reshape::melt(PIPO.q.rwl.wide, id.vars = "series")
+colnames(PIPO.q.rwl.m) <- c("series","year", "rwl")
+
+PIED.q.rwl.wide <- PIED.q.rwl.wide %>% filter(series %in% all.trees.info$TREE.ID.)
+colnames(PIED.q.rwl.wide)[2:length(PIED.q.rwl.wide)] <- 1895:1996
+PIED.q.rwl.m <- reshape::melt(PIED.q.rwl.wide, id.vars = "series")
+colnames(PIED.q.rwl.m) <- c("series","year", "rwl")
+
+PSME.q.rwl.wide <- PSME.q.rwl.wide %>% filter(series %in% paste0(0,all.trees.info$TREE.ID.))
+colnames(PSME.q.rwl.wide)[2:length(PSME.q.rwl.wide)] <- 1772:1997
+PSME.q.rwl.m <- reshape::melt(PSME.q.rwl.wide, id.vars = "series")
+colnames(PSME.q.rwl.m) <- c("series","year", "rwl")
+
+PIED.1 <- PIED.q.rwl.m %>% filter(series %in% "1058501")
+PIED.2 <- PIED.q.rwl.m %>% filter(series %in% "1060201")
+PIED.3 <- PIED.q.rwl.m %>% filter(series %in% "1060204")
+PIED.4 <- PIED.q.rwl.m %>% filter(series %in% "5177904")
+PIED.5 <- PIED.q.rwl.m %>% filter(series %in% "11001304")
+PIED.6 <- PIED.q.rwl.m %>% filter(series %in% "17095001")
+
+
+PIPO.1 <- PIPO.q.rwl.m %>% filter(series %in% "X61" & !is.na(rwl))
+PIPO.2 <- PIPO.q.rwl.m %>% filter(series %in% "X154" & !is.na(rwl))
+PIPO.3 <- PIPO.q.rwl.m %>% filter(series %in% "X190" & !is.na(rwl))
+
+
+
+PSME.1 <- PSME.q.rwl.m %>% filter(series %in% "01112001" & !is.na(rwl))
+PSME.2 <- PSME.q.rwl.m %>% filter(series %in% "03333040" & !is.na(rwl))
+PSME.3 <- PSME.q.rwl.m %>% filter(series %in% "07015504" & !is.na(rwl))
+
+source("R/plotRings2.R")
+# Six PIED trees:
+plotRings2(year = as.integer(as.character(PIED.1$year)), trwN= PIED.1$rwl,  saveGIF = TRUE, species = "Pinus edulis", treeid = "1058501", fname = "PIED_1058501_growth")
+plotRings2(year = as.integer(as.character(PIED.2$year)), trwN= PIED.2$rwl,  saveGIF = TRUE, species = "Pinus edulis", treeid = "1060201", fname = "PIED_1060201_growth")
+plotRings2(year = as.integer(as.character(PIED.3$year)), trwN= PIED.3$rwl,  saveGIF = TRUE, species = "Pinus edulis", treeid = "1060204",fname = "PIED_1060204_growth")
+plotRings2(year = as.integer(as.character(PIED.4$year)), trwN= PIED.4$rwl,  saveGIF = TRUE, species = "Pinus edulis", treeid = "5177904", fname = "PIED_5177904_growth")
+plotRings2(year = as.integer(as.character(PIED.5$year)), trwN= PIED.5$rwl,  saveGIF = TRUE, species = "Pinus edulis", treeid = "11001304", fname = "PIED_11001304_growth")
+plotRings2(year = as.integer(as.character(PIED.6$year)), trwN= PIED.6$rwl,  saveGIF = TRUE, species = "Pinus edulis", treeid = "17095001",fname = "PIED_17095001_growth")
+
+#
+
+# three PIPO trees:
+plotRings2(year = as.integer(as.character(PIPO.1$year)), trwN= PIPO.1$rwl,  saveGIF = TRUE, species = "Pinus ponderosa", treeid = "X61", fname = "PIPO_X61_growth")
+plotRings2(year = as.integer(as.character(PIPO.2$year)), trwN= PIPO.2$rwl,  saveGIF = TRUE, species = "Pinus ponderosa", treeid = "X154", fname = "PIPO_X154_growth")
+plotRings2(year = as.integer(as.character(PIPO.3$year)), trwN= PIPO.3$rwl,  saveGIF = TRUE, species = "Pinus ponderosa", treeid = "X190", fpath = "growth_gifs",fname = "PIPO_X190_growth")
+#plotRings2(year = PIED.1$year, trwN= PIPO.4$rwl,  saveGIF = TRUE, species = "Pinus ponderosa", treeid = "1058501", fname = "PIED_1058501_growth.gif")
+
+
+# Three PSME trees:
+plotRings2(year = as.integer(as.character(PSME.1$year)), trwN = PSME.1$rwl,  saveGIF = TRUE, species = "Pseudotsuga menziesii", treeid = "1112001", fname = "PSME_1112001_growth")
+plotRings2(year = as.integer(as.character(PSME.2$year)), trwN = PSME.2$rwl,  saveGIF = TRUE, species = "Pseudotsuga menziesiiPinus ponderosa", treeid = "3333040", fname = "PSME_3333040_growth")
+plotRings2(year = as.integer(as.character(PSME.3$year)), trwN = PSME.3$rwl,  saveGIF = TRUE, species = "Pseudotsuga menziesii", treeid = "7015504", fpath = "growth_gifs",fname = "PSME_70155040_growth")
